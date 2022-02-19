a48728d906de3aee0bc4c762683a2344b14313f4
pK <pave.kruchin@gmail.com>
2022-02-19 18:48:06 +0200

    create folders for project

---
+++ MyScheduled/MyScheduled/Controllers/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MyScheduled
+//
+//  Created by Pavel Olegovich on 19.02.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .red
+    }
+
+
+}
+
